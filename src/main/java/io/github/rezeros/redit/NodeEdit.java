6d7652b1282e10b7884893b38e9b394d465af6e9
lijie78 <lijie78@58.com>
2022-11-04 20:31:38 +0800

    补充注释

--- src/main/java/io/github/rezeros/redit/NodeEdit.java
+++ src/main/java/io/github/rezeros/redit/NodeEdit.java
@@ -22,26 +22,35 @@ public class NodeEdit extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
+        // 获取整个项目
         Project project = e.getProject();
+        // 获取当前编辑器
         final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+        // 获取这个编辑器的文档 比如现在正在聚焦的这个文件
         Document document = editor.getDocument();
+
+        // 文件的内容都是字符串，直接用会很麻烦，还得自己搞parser
         PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
         if (!(psiFile instanceof PsiJavaFile)) {
             return;
         }
-
+        // idea plugin 提供了一些这种默认的parser，直接转就可以
         PsiJavaFile javaFile = (PsiJavaFile) psiFile;
+
+        // 转成 java 后就能拿到 class
         PsiClass[] classesInFile = javaFile.getClasses();
 
+        // 开个 element 工厂准备干活 element 就是节点，比如类节点，字段节点，注解节点等等
         PsiElementFactory elementFactory = JavaPsiFacade.getElementFactory(project);
 
-
+        // 这里没细查，估计是和android那种怕阻塞 UI 主线程之类的
         WriteCommandAction.runWriteCommandAction(project, () -> {
             for (PsiClass psiClass : classesInFile) {
                 PsiField[] fieldList = psiClass.getAllFields();
                 int i = 1;
                 for (PsiField field : fieldList) {
                     PsiAnnotation annotation = elementFactory.createAnnotationFromText(String.format("@SCFMember(orderId = %d)", i++), field);
+                    // 这里 add 之后就会进行绘制，add 即刻触发写操作，不需要 write 之类的接口
                     psiClass.addBefore(annotation, field);
                 }
             }
